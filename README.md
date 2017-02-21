e0cfd1ecbed523bfd17672e183d29e1c82807105
Rafael Quines <rafael.quines@totvs.com.br>
2017-02-21 10:42:22 -0300

    Change to pigpio

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # inputmonitor
-An Input Monitor for GPIOs on Raspberry PI with Debounce
+An Input Monitor for GPIOs on Raspberry PI with Node.js using pigpio
 
 ## Basic Usage
 
@@ -8,23 +8,22 @@ An Input Monitor for GPIOs on Raspberry PI with Debounce
 var InputMonitor = require('inputmonitor');
 
 // Define constants
-var debounce = 15;
 var gpioNum = 19;
 var inputName = 'Input1';
-var gpioNum = 1;
+var inputNum = 1;
 
 // Function to get input status changed
-function inputStatusChanged(err, number, name, value) {
+function inputStatusChanged(number, name, value) {
     console.log(name + '[' + number + '] (' + (value ? 'ACTIVED' : 'DEACTIVED') + ')');
 }
 
-// Initialize InputMonitor with GPIO number, a number, a name and a debounce time
-var inputMonitor = new InputMonitor(gpioNum, gpioNum, inputName, debounce);
+// Initialize InputMonitor with GPIO number, a number, a name
+var inputMonitor = new InputMonitor(gpioNum, Gpio.PUD_DOWN, inputNum, inputName);
 
 // Set funcion callback
 inputMonitor.onInputChange(inputStatusChanged);
 
 // Start watch
-inputMonitor.start()
+inputMonitor.start();
 
 ```
--- inputmonitor.js
+++ inputmonitor.js
@@ -1,47 +1,24 @@
 "use strict";
-var Gpio = require('onoff').Gpio;
+var Gpio = require('pigpio').Gpio;
 
-Gpio.prototype.watchFilter = function (callback, debounce) {
-    const that = this;
-    if (!this.watchFilterData) {
-        const d = this.watchFilterData = {
-            oldValue: undefined,
-            timer: undefined,
-            listener: []
-        };
-        this.watch(function (err, value) {
-            if (err) {
-                if (d.listener.length == 0) throw err;
-                d.listener.forEach(l => l.call(that, err, value));
-                return;
-            }
-            clearTimeout(d.timer);
-            d.timer = setTimeout(function () {
-                if (d.oldValue === value) {
-                    return;
-                }
-                d.oldValue = value;
-                d.listener.forEach(l => l.call(that, undefined, value));
-            }, debounce);
-        });
-    }
-    this.watchFilterData.listener.push(callback);
-};
-
-function InputMonitor(pin, number, name, debounce) {
-    this._pin = pin;
-    this._number = number;
-    this._name = name;
-    this._debounce = debounce ? 0 : debounce;
-    this._input = new Gpio(pin, 'in', 'both');
+function InputMonitor(gpioNum, pullUpDown, inNumber, inName) {
+    this._gpioNum = gpioNum;
+    this._inNumber = inNumber;
+    this._inName = inName;
+    this._input = new Gpio(this._gpioNum, {
+        mode: Gpio.INPUT,
+        pullUpDown: pullUpDown,
+        edge: Gpio.EITHER_EDGE
+    });
 }
 
 InputMonitor.prototype.start = function() {
     var that = this;
-    this._input.watchFilter(function (err, value) {
-        if(typeof that._callback === 'function')
-            that._callback(err, that._number, that._name, value);
-    }, this._debounce);
+    this._input.on('interrupt', function(level) {
+        if(typeof that._callback === 'function') {
+            that._callback(that._inNumber, that._inName, level);
+        }
+    });
 };
 
 InputMonitor.prototype.onInputChange = function(callback) {
