8b0e1986f6183b4a63e15504513c3d235b5fe940
Rafael Quines <rafaelquines@gmail.com>
2017-06-29 22:50:03 -0300

    added timeout

--- inputmonitor.js
+++ inputmonitor.js
@@ -1,21 +1,22 @@
 "use strict";
 var Gpio = require('pigpio').Gpio;
 
-function InputMonitor(gpioNum, pullUpDown, inNumber, inName) {
+function InputMonitor(gpioNum, pullUpDown, inNumber, inName, inTimeout) {
     this._gpioNum = gpioNum;
     this._inNumber = inNumber;
     this._inName = inName;
     this._input = new Gpio(this._gpioNum, {
         mode: Gpio.INPUT,
         pullUpDown: pullUpDown,
-        edge: Gpio.EITHER_EDGE
+        edge: Gpio.EITHER_EDGE,
+        timeout: inTimeout ? 0 : inTimeout
     });
 }
 
 InputMonitor.prototype.start = function() {
     var that = this;
     this._input.on('interrupt', function(level) {
-        if(typeof that._callback === 'function') {
+        if (typeof that._callback === 'function') {
             that._callback(that._inNumber, that._inName, level);
         }
     });
