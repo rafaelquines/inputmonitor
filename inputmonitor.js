167d0ecbbd2678382169485ff822620801bd1f66
Rafael Quines <rafael.quines@totvs.com.br>
2017-07-03 16:12:53 -0300

    fix timeout

--- inputmonitor.js
+++ inputmonitor.js
@@ -5,26 +5,26 @@ function InputMonitor(gpioNum, pullUpDown, inNumber, inName, inTimeout) {
     this._gpioNum = gpioNum;
     this._inNumber = inNumber;
     this._inName = inName;
-    this._inTimeout = inTimeout ? 0 : inTimeout;
+    this._inTimeout = inTimeout ? inTimeout : 0;
     this._input = new Gpio(this._gpioNum, {
         mode: Gpio.INPUT,
         pullUpDown: pullUpDown,
         edge: Gpio.EITHER_EDGE,
-        timeout: inTimeout ? 0 : inTimeout
+        timeout: inTimeout ? inTimeout : 0
     });
-    this._lastInterruptTime = 0;
+    // this._lastInterruptTime = 0;
 }
 
 InputMonitor.prototype.start = function () {
     var that = this;
     this._input.on('interrupt', function (level) {
-        var interruptTime = Date.now();
-        if ((interruptTime - that._lastInterruptTime) >= that._inTimeout) {
-            if (typeof that._callback === 'function') {
-                that._callback(that._inNumber, that._inName, level);
-            }
+        // var interruptTime = Date.now();
+        // if ((interruptTime - that._lastInterruptTime) >= that._inTimeout) {
+        if (typeof that._callback === 'function') {
+            that._callback(that._inNumber, that._inName, level);
         }
-        that._lastInterruptTime = interruptTime;
+        // }
+        // that._lastInterruptTime = interruptTime;
     });
 };
 
