dceebf1a42604ef833e7365869e991e83f3b1eb4
Rafael Quines <rafael.quines@totvs.com.br>
2017-11-01 14:16:23 -0200

    fix

--- inputmonitor.js
+++ inputmonitor.js
@@ -1,5 +1,5 @@
 "use strict";
-var Gpio = process.env.NODE_ENV !== 'production' ? require('pigpio-mock').Gpio : require('pigpio').Gpio;
+var Gpio = !process.env.NODE_ENV || process.env.NODE_ENV == 'production' ? require('pigpio').Gpio : require('pigpio-mock').Gpio;
 
 function InputMonitor(gpioNum, pullUpDown, inNumber, inName, inTimeout) {
     this._gpioNum = gpioNum;
